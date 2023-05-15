0ab1032aeec76bb0d4afae1a44ce7b8f4704ca7b
kevinsavala <118188106+kevinsavala@users.noreply.github.com>
2023-05-15 11:58:33 -0600

    a
    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,45 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent }   from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CarteleraComponent } from './cartelera/cartelera.component';
+import { AlimentosComponent } from './alimentos/alimentos.component';
+import { AlimentoService } from './alimentos/alimentos.service';
+import { UbicanosComponent } from './ubicanos/ubicanos.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { CarteleraService } from './cartelera/cartelera.service';
 import { CalendarModule } from 'primeng/calendar';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReservarComponent } from './reservar/reservar.component';
+import { SeguroPipe } from './alimentos/seguro.pipe';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { MessagesModule } from 'primeng/messages';
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     CalendarModule,
-    FormsModule
+    ReactiveFormsModule,
+    CommonModule,
+    TableModule,
+    MessagesModule,
   ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CarteleraComponent,
+    AlimentosComponent,
+    UbicanosComponent,
+    PerfilComponent,
+    ReservarComponent,
+    SeguroPipe,
+  ],
+  bootstrap: [AppComponent],
+  providers: [AlimentoService, CarteleraService],
 })
-
-export class AppModule { }
+export class AppModule {}
