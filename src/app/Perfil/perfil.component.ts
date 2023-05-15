eec5888b11d74ec3b31fbfbd7b6a5021f60e8373
kevinsavala <118188106+kevinsavala@users.noreply.github.com>
2023-05-15 12:00:35 -0600

    Add files via upload

---
+++ src/app/Perfil/perfil.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Message } from 'primeng/api';
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css'],
+})
+export class PerfilComponent implements OnInit {
+  username: string;
+  email: string;
+  password: string;
+  mensaje: string = '';
+  reservas: any[];
+  
+  constructor(private router: Router) {
+    const reservasLocalStorage = JSON.parse(localStorage.getItem('reserva'));
+    this.reservas = reservasLocalStorage ? reservasLocalStorage : [];
+  }
+  ngOnInit() {
+    
+  }
+  registro() {
+    // validamos que todos los campos estén llenos
+    if (!this.username || !this.email || !this.password) {
+      this.mensaje = 'Por favor rellene todos los campos.';
+      return;
+    }
+
+    // validamos si el usuario ya existe en localStorage
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    if (usuarios.some((usuario) => usuario.username === this.username)) {
+      this.mensaje = 'Este usuario ya existe, por favor intente con otro.';
+      return;
+    }
+
+    // guardamos el nuevo usuario en localStorage
+    usuarios.push({
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    });
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    // mostramos un mensaje de éxito
+    this.mensaje = 'Usuario creado exitosamente.';
+    
+  }
+
+  iniciar() {
+    // validamos que todos los campos estén llenos
+    if (!this.username || !this.password) {
+      this.mensaje = 'Por favor rellene todos los campos.';
+      return;
+    }
+
+    // validamos si el usuario existe en localStorage
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const usuario = usuarios.find(
+      (usuario) =>
+        usuario.username === this.username && usuario.password === this.password
+    );
+    if (!usuario) {
+      this.mensaje =
+        'Nombre de usuario o contraseña incorrectos, por favor intente de nuevo.';
+      return;
+    }
+
+    // creamos la sesión del usuario en localStorage
+    localStorage.setItem('sesion', JSON.stringify(usuario));
+
+    // mostramos un mensaje de éxito
+    this.mensaje = 'Inicio de sesión exitoso.';
+  }
+}
